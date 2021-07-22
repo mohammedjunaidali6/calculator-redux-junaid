3aa581b01dfa12eddd2e232c4019b10859f7427b
junaid <junaidali1507@gmail.com>
2021-07-22 19:16:57 +0530

    added files

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { increaseCounter, decreaseCounter, multiplyCounter, divisionCounter } from './redux/Counter/counter.actions';
+import { Button } from 'react-bootstrap';
+import './App.css';
+
+function App(props) {
+  return (
+    <div className="App">
+      <h3>Count : {props.count}</h3>
+      <Button className="btn1" onClick={()=> props.increaseCounter()}>Addition</Button><br/><br/>
+      <Button className="btn2" onClick={()=> props.decreaseCounter()}>Subtraction</Button><br/><br/>
+      <Button className="btn3" onClick={()=> props.multiplyCounter()}>Multiply</Button><br/><br/>
+      <Button className="btn4" onClick={()=> props.divisionCounter()}>Division</Button><br/><br/>
+    </div>
+  );
+}
+
+const mapStateToProps = state => {
+ return{
+  count: state.counter.count,
+ }
+}
+const mapDispatchToProps = dispatch => {
+  return{
+    increaseCounter:() =>dispatch(increaseCounter()),
+    decreaseCounter:() =>dispatch(decreaseCounter()),
+    multiplyCounter:()=> dispatch(multiplyCounter()),
+    divisionCounter:()=> dispatch(divisionCounter()),
+
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
---
+++ src/redux/Counter/counter.types.js
@@ -0,0 +1,5 @@
+export const INCREMENT = 'INCREMENT';
+export const DECREMENT = 'DECREMENT';
+export const MULTIPLICATION = 'MULTIPLICATION';
+export const DIVISION = 'DIVISION';
+
